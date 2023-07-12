8a179c99c584d2399f3da9a74c64825870fb37c0
Ashley Le <53335012+lediemhang87@users.noreply.github.com>
2023-07-11 20:23:03 -0700

    Done with to-do-list

--- src/components/dashboard/dashboard-mytodoitems.tsx
+++ src/components/dashboard/dashboard-mytodoitems.tsx
@@ -1,46 +1,177 @@
-import '../styles2.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPen, faTrash, faGripVertical, faSquare, faTriangleExclamation, faCheck } from '@fortawesome/free-solid-svg-icons'
+import '../styles2.scss';
+import { DndContext, closestCenter, DragEndEvent } from '@dnd-kit/core';
+import { arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
+import { useState } from 'react';
+import SortableItem from './todo-item';
+import { v4 as uuid } from 'uuid';
 
+
+type TodoItem = {
+  id: string;
+  title: string;
+  date: string;
+  time: string;
+  status?: string;
+  isChecked: boolean;
+};
 
 const DashboardMyToDoItems: React.FC = () => {
-    const todoList = [
-        {'title': 'Complete project Monday', 'date': '2023-12-26', 'time': '07:15:00', 'status': 'latest-to-do'},
-        {'title': 'Complete project Tuesday', 'date': '2023-12-27', 'time': '07:15:00','status': 'latest-finished'},
-        {'title': 'Complete project Wed', 'date': '2023-12-28', 'time': '07:15:00'},
-        {'title': 'Complete project Thursday', 'date': '2023-12-29', 'time': '07:15:00'},
-        
-    ]
-    return(
-        <div className='myTodoItems'>
-            <div className='myTodo-title-div'>
-                <div className='title-left'> My To Do Items </div>
-                <div className='title-right'> View All + Add To Do </div>
-            </div>
-            {todoList.map((todo, index) => (
-                <div key={index} className='todo-item' style={{ borderTopWidth: index === 0 ? '0' : '1px' }}>
-                    {todo.status === 'latest-to-do' && 
-                        <div className='status orange'> <FontAwesomeIcon className='icon ' icon={faTriangleExclamation} /> Latest to do's</div>}
-                    {todo.status === 'latest-finished' && 
-                    <div className='status green'> <FontAwesomeIcon className='icon ' icon={faCheck} /> Latest finished to do's</div>}
-                    <div className='title-div'>
-                        <FontAwesomeIcon className='icon ' icon={faGripVertical} />
-                        <FontAwesomeIcon className='icon ' icon={faSquare} />
-                        <div className='title'>{todo.title}</div>
-                        <div className='icons '>
-                            <FontAwesomeIcon className='icon red' icon={faTrash} />
-                            <FontAwesomeIcon className='icon blue ' icon={faPen} />
-                        </div>
-                    </div>
+  const initialTodoList: TodoItem[] = [
+    { id: '0', title: 'Complete project Monday', date: '2023-12-26', time: '07:15:00', status: 'latest-to-do', isChecked: false},
+    { id: '1', title: 'Complete project Tuesday', date: '2023-12-27', time: '07:15:00', status: 'latest-finished',  isChecked: false},
+    { id: '2', title: 'Complete project Wed', date: '2023-12-28', time: '07:15:00', 'status': '',  isChecked: false},
+    { id: '3', title: 'Complete project Thursday', date: '2023-12-29', time: '07:15:00', 'status': '',  isChecked: false }
+  ];
 
-                    <div className='date-and-time'>
-                        {todo.date}  {todo.time}
-                    </div>
-                </div>
-            ))}
+  const [todoList, setTodoList] = useState(initialTodoList);
+  const [showAddForm, setShowAddForm] = useState(false);
+  const [newTodo, setNewTodo] = useState<TodoItem>({ id: '0', title: 'Complete project Monday', date: '2023-12-26', time: '07:15:00', status: '',  isChecked: false});
 
-            
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (over !== null && active?.id !== over.id) {
+      setTodoList((items: TodoItem[]) => {
+        const activeIndex = items.findIndex((item) => item.id === active?.id);
+        const overIndex = items.findIndex((item) => item.id === over.id);
+        return arrayMove(items, activeIndex, overIndex);
+      });
+    }
+  };
+
+  const handleDeleteTodo = (id: string) => {
+    setTodoList((prevTodoList) => prevTodoList.filter((todo) => todo.id !== id));
+  };
+
+  
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNewTodo((prevTodo) => ({
+      ...prevTodo,
+      [name]: value
+    }));
+  };
+
+  const handleCheck = (id: string) => {
+    // Find the index of the checked item
+    const checkedItemIndex = todoList.findIndex((todo) => todo.id === id);
+  
+    // Check if the item is already crossed out
+    if (todoList[checkedItemIndex].status === 'latest-finished') {
+      // Remove the status from the checked item
+      const updatedTodoList = todoList.map((todo) =>
+        todo.id === id ? { ...todo, isChecked: false, status: '' } : todo
+      );
+      setTodoList(updatedTodoList);
+    } else {
+      // Update the isChecked property of the todo item
+      const updatedTodoList = todoList.map((todo) =>
+        todo.id === id ? { ...todo, isChecked: !todo.isChecked } : todo
+      );
+  
+      // Find the index of the old 'latest-finished' item
+      const oldLatestFinishedIndex = updatedTodoList.findIndex(
+        (todo) => todo.status === 'latest-finished'
+      );
+  
+      // Create a copy of the updated todo list
+      const updatedTodoListWithStatus = [...updatedTodoList];
+  
+      // Remove the 'latest-finished' status from the old item if it exists
+      if (oldLatestFinishedIndex !== -1) {
+        updatedTodoListWithStatus[oldLatestFinishedIndex] = {
+          ...updatedTodoListWithStatus[oldLatestFinishedIndex],
+          status: '',
+        };
+      }
+  
+      // Set the status of the checked item to 'latest-finished'
+      updatedTodoListWithStatus[checkedItemIndex] = {
+        ...updatedTodoListWithStatus[checkedItemIndex],
+        status: 'latest-finished',
+      };
+  
+      // Update the todo list with the updated status
+      setTodoList(updatedTodoListWithStatus);
+    }
+  };
+  
+  
+
+const handleAddTodo = (e: React.FormEvent) => {
+  e.preventDefault();
+  if (newTodo.title && newTodo.date && newTodo.time) {
+    // Remove the 'latest-to-do' status from the old item if it exists
+    const updatedTodoList = todoList.map((todo) => {
+      if (todo.status == 'latest-to-do') {
+        return {
+          ...todo,
+          status: '',
+        };
+      }
+      return todo;
+    });
+
+    const newId = uuid();
+
+    const updatedNewTodo = { ...newTodo, status: 'latest-to-do', id: newId };
+    updatedTodoList.unshift(updatedNewTodo);
+
+    // Update the todo list
+    setTodoList(updatedTodoList);
+
+    setNewTodo(updatedNewTodo);
+    setShowAddForm(false);
+    console.log(todoList)
+  }
+};
+  const handleCancelAdd = () => {
+    setNewTodo({ id: '', title: '', date: '', time: '', status: 'latest-to-do', isChecked: false });
+    setShowAddForm(false);
+  };
+
+  const handleUpdateTodo = (updatedTodo: TodoItem) => {
+    setTodoList((prevTodoList) => {
+      const updatedTodoList = prevTodoList.map((todo) => {
+        if (todo.id === updatedTodo.id) {
+          return updatedTodo;
+        }
+        return todo;
+      });
+      return updatedTodoList;
+    });
+  };
+
+  return (
+    <div className='myTodoItems'>
+      <div className='myTodo-title-div'>
+        <div className='title-left'> My To Do Items </div>
+        <div className='title-right'>
+          View All + 
+          <button onClick={() => setShowAddForm(true)}>Add Item</button>
         </div>
-    )
-}
-export default DashboardMyToDoItems
+      </div>
+
+      {showAddForm && (
+        <form onSubmit={handleAddTodo}>
+          <input type='text' name='title' placeholder='Title' value={newTodo.title} onChange={handleInputChange} required />
+          <input type='date' name='date' value={newTodo.date} onChange={handleInputChange} required />
+          <input type='time' name='time' value={newTodo.time} onChange={handleInputChange} required />
+          <button type='submit'>Add To Do</button>
+          <button type='button' onClick={handleCancelAdd}>Cancel</button>
+        </form>
+      )}
+
+      <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+        <SortableContext items={todoList} strategy={verticalListSortingStrategy}>
+          {todoList.map((todo, index) => (
+            <SortableItem key={todo.id} todo={todo} index={index} onDelete={handleDeleteTodo} onUpdateTodo={handleUpdateTodo} onCheck={handleCheck}/>
+
+          ))}
+        </SortableContext>
+      </DndContext>
+    </div>
+  );
+};
+
+export default DashboardMyToDoItems;
---
+++ src/components/dashboard/todo-item.tsx
@@ -0,0 +1,121 @@
+import { useState, useEffect } from 'react';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faTrash, faGripVertical, faTriangleExclamation, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+type TodoItem = {
+  id: string;
+  title: string;
+  date: string;
+  time: string;
+  status?: string;
+  isChecked: boolean;
+};
+
+type SortableItemProps = {
+  todo: TodoItem;
+  index: number;
+  onDelete: (id: string) => void;
+  onUpdateTodo: (updatedTodo: TodoItem) => void;
+  onCheck: (id: string, isChecked: boolean) => void;
+};
+
+const SortableItem: React.FC<SortableItemProps> = ({ todo, index, onDelete, onUpdateTodo, onCheck }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: todo.id });
+  const [editMode, setEditMode] = useState(false);
+  const [editedTodo, setEditedTodo] = useState(todo);
+
+  useEffect(() => {
+    setEditedTodo(todo);
+  }, [todo]);
+
+  const handleEdit = () => {
+    setEditMode(true);
+  };
+
+  const handleCancel = () => {
+    setEditedTodo(todo);
+    setEditMode(false);
+  };
+
+  const handleSave = () => {
+    console.log('Save todo:', editedTodo);
+    setEditMode(false);
+    onUpdateTodo(editedTodo);
+  };
+
+  const handleDelete = () => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this item?');
+    if (confirmDelete) {
+      onDelete(todo.id);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setEditedTodo((prevTodo) => ({
+      ...prevTodo,
+      [name]: value,
+    }));
+  };
+
+  const handleCheckboxChange = () => {
+    const updatedTodo = { ...editedTodo, isChecked: !editedTodo.isChecked };
+    setEditedTodo(updatedTodo);
+    onCheck(updatedTodo.id, updatedTodo.isChecked);
+  };
+
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style}>
+      <div className='todo-item' style={{ borderTopWidth: index === 0 ? '0' : '1px' }}>
+      {todo.status === 'latest-to-do' && (
+          <div className='status orange'>
+            <FontAwesomeIcon className='icon ' icon={faTriangleExclamation} /> Latest to do's
+          </div>
+        )}
+        {todo.status === 'latest-finished' && (
+          <div className='status green'>
+            <FontAwesomeIcon className='icon ' icon={faCheck} /> Latest finished to do's
+          </div>
+        )}
+        {editMode ? (
+          <>
+            <input className='border width-100 margin-bottom-10' type='text' name='title' value={editedTodo.title} onChange={handleInputChange} />
+            <input className='border width-50 margin-bottom-10 ' type='date' name='date' value={editedTodo.date} onChange={handleInputChange} />
+            <input className='border width-50 margin-bottom-10' type='time' name='time' value={editedTodo.time} onChange={handleInputChange} />
+            <button className='green-button padding-10 margin-right-10 border-radius-10' onClick={handleSave}>
+              Save
+            </button>
+            <button className='red-button padding-10  border-radius-10 ' onClick={handleCancel}>
+              Cancel
+            </button>
+          </>
+        ) : (
+          <>
+            <div className='title-div'>
+              <FontAwesomeIcon className='icon ' icon={faGripVertical} {...attributes} {...listeners} />
+              <input className='checkbox' type='checkbox' checked={todo.isChecked} onChange={handleCheckboxChange} />
+              <div className={`title ${todo.isChecked ? 'crossed' : ''}`}>{editedTodo.title}</div>
+              <div className='icons '>
+                <FontAwesomeIcon className='icon red' icon={faTrash} onClick={handleDelete} />
+                <FontAwesomeIcon className='icon blue' icon={faPen} onClick={handleEdit} />
+              </div>
+            </div>
+            <div className='date-and-time'>
+              {editedTodo.date} {editedTodo.time}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SortableItem;
